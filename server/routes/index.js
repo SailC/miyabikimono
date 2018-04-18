a59a13bcdf97a443c7affb42784b059b8a657d83
Qihang <chenqi@us.ibm.com>
2018-04-18 15:35:51 -0400

    add fb sdk

--- server/routes/index.js
+++ server/routes/index.js
@@ -88,6 +88,29 @@ function renderPage(appHtml) {
       </head>
 
       <body>
+
+        <script>
+          window.fbAsyncInit = function() {
+            FB.init({
+              appId      : '443330156103640',
+              cookie     : true,
+              xfbml      : true,
+              version    : 'v2.12'
+            });
+
+            FB.AppEvents.logPageView();
+
+          };
+
+          (function(d, s, id){
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement(s); js.id = id;
+             js.src = "https://connect.facebook.net/en_US/sdk.js";
+             fjs.parentNode.insertBefore(js, fjs);
+           }(document, 'script', 'facebook-jssdk'));
+        </script>
+
         <div id="app" itemscope="" itemtype="http://schema.org/WebPage">${appHtml}</div>
         <script src="https://cf-4053.kxcdn.com/conversational-form/0.9.6/conversational-form.min.js"></script>
         <script src="/bundle.js"></script>
